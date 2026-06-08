d5e6422cd1b39caa8ed7128123d4a490774f497a
Joseph MacDonald <wexasmacdonald@gmail.com>
2026-06-08 16:40:41 -0400

    Remove public placeholder assets

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/gallery",
+      disallow: ["/gallery", "/placeholder*"],
     },
     sitemap: `${SITE_URL}/sitemap.xml`,
     host: SITE_URL,
